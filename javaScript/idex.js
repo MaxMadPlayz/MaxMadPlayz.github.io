c54d6cd9779f0b4884bee0955463944bb2142384
MaxMadPlayz <157627252+MaxMadPlayz@users.noreply.github.com>
2024-02-10 16:21:06 +0100

    Add files via upload

---
+++ javaScript/idex.js
@@ -0,0 +1,13 @@
+
+var name = "MAX"
+var age = 12
+var likesSpeedCubing = true
+console.log(`hello`)
+console.log(`u r ${age} years old.`)
+console.log(typeof name)
+
+var func = function(newText){
+    document.getElementById("h1").textContent = newText
+    console.log(newText)
+}
+    
